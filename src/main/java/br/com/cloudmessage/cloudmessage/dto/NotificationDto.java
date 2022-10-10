34f422542bd679a4bc6d55fb844710fef517450d
Roberto Rosa <roberto.rosa7@gmail.com>
2022-10-10 12:20:24 -0300

    image-3362 - Coverage

--- src/main/java/br/com/cloudmessage/cloudmessage/dto/NotificationDto.java
+++ src/main/java/br/com/cloudmessage/cloudmessage/dto/NotificationDto.java
@@ -3,7 +3,6 @@ package br.com.cloudmessage.cloudmessage.dto;
 import com.google.gson.Gson;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.io.Serializable;
 
@@ -11,27 +10,25 @@ import java.io.Serializable;
 @Setter
 public class NotificationDto implements Serializable {
 	private Notification notification;
-	@Value("${image.path.default}")
-	private String imagePathDefault;
 
-	public NotificationDto(String title, String body, String icon) {
-		this.notification = new Notification();
-		System.out.println(this.imagePathDefault);
-		this.notification.setIcon(this.imagePathDefault);
-		this.notification.setTitle(title);
-		this.notification.setBody(body);
+	public NotificationDto(Notification notification) {
+		this.notification = notification;
 	}
 
 	public String serialize() {
 		return new Gson().toJson(this);
 	}
 
-	public class NotificationAction {
+	@Getter
+	@Setter
+	public static class NotificationAction {
 		private String action;
 		private String title;
 	}
 
-	public class Dir {
+	@Getter
+	@Setter
+	public static class Dir {
 		String auto = "auto";
 		String ltr = "ltr";
 		String rtl = "rtl";
@@ -39,7 +36,7 @@ public class NotificationDto implements Serializable {
 
 	@Getter
 	@Setter
-	public class Notification {
+	public static class Notification {
 		private String icon;
 		private String title;
 		private String body;
--- src/main/java/br/com/cloudmessage/cloudmessage/service/MessageService.java
+++ src/main/java/br/com/cloudmessage/cloudmessage/service/MessageService.java
@@ -36,6 +36,22 @@ public class MessageService {
 	private PushService pushService;
 	private List<Subscription> subscriptions = new ArrayList<>();
 
+	private static List<NotificationDto.NotificationAction> getNotificationActions() {
+		List<NotificationDto.NotificationAction> actionList = new ArrayList<>();
+
+		NotificationDto.NotificationAction action = new NotificationDto.NotificationAction();
+		action.setTitle("Excluir");
+		action.setAction("delete");
+
+		NotificationDto.NotificationAction action2 = new NotificationDto.NotificationAction();
+		action2.setTitle("Confirmar");
+		action2.setAction("confirm");
+
+		actionList.add(action);
+		actionList.add(action2);
+		return actionList;
+	}
+
 	@PostConstruct // used when call service
 	private void init() throws GeneralSecurityException {
 		Security.addProvider(new BouncyCastleProvider());
@@ -67,14 +83,31 @@ public class MessageService {
 	@Scheduled(fixedRate = 30000)
 	private void sendNotifications() {
 		System.out.println("Sending notifications to all subscribers: " + this.subscriptions.size());
-		NotificationDto notificationDto = new NotificationDto(
-			"Roberto",
-			"Teste de Mensagem",
-			this.icon
-		);
+		NotificationDto notificationDto = new NotificationDto(getNotification());
 
 		System.out.println(notificationDto.serialize());
-
 		subscriptions.forEach(subscription -> sendNotification(subscription, notificationDto.serialize()));
+	}
+
+	private NotificationDto.Notification getNotification() {
+		NotificationDto.Notification notification = new NotificationDto.Notification();
+		NotificationDto.NotificationAction[] actions = this.convert(getNotificationActions());
+
+		notification.setTitle("test");
+		notification.setBody("test body");
+		notification.setIcon("http://localhost:8081/file/image/kakashi.jpg");
+		notification.setActions(actions);
+		return notification;
+	}
+
+	private NotificationDto.NotificationAction[] convert(List<NotificationDto.NotificationAction> list) {
+		NotificationDto.NotificationAction[] actions = new NotificationDto.NotificationAction[list.size()];
+
+		for (int i = 0; i < list.size(); i++) {
+			NotificationDto.NotificationAction action = list.get(i);
+			actions[i] = action;
+		}
+
+		return actions;
 	}
 }
